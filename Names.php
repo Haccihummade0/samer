a5301606eaf917bfa95ff808f38b7c48199a3d54
Haccihummade0 <77897693+Haccihummade0@users.noreply.github.com>
2022-01-22 18:25:54 +0200

    Create Names.php

---
+++ Names.php
@@ -0,0 +1,15 @@
+<?php
+for ($i=0; $i < 16; $i++) { 
+$names = array("حسن","محمد","علي","احمد","خالد","ياسر","جاسم","خضر","خالد","سماعين","بسام","ابراهيم","عبود","عزيز","زهير","محمود",);
+$url = "http://www.123.com/login?username=".$arrayName[array_rand($arrayName)];
+$html = file_get_html($url);
+$new = $html->find("div.alert3");
+if ($new) {
+    echo "FALSE"."\n";
+    echo $url;
+    echo "<br>";
+}else{
+	echo "Done";
+}
+}
+?>
