83ac60497019d5572c6f9b6a5c29d382d5002369
Haccihummade0 <77897693+Haccihummade0@users.noreply.github.com>
2022-01-22 18:37:34 +0200

    Update Names.php

--- Names.php
+++ Names.php
@@ -1,6 +1,6 @@
 <?php
 require "simple_html_dom.php";
-for ($i=0; $i < 16; $i++) { 
+for ($i=0; $i < 20; $i++) { 
 $arrayName = array("حسن","محمد","علي","احمد","خالد","ياسر","جاسم","خضر","خالد","سماعين","بسام","ابراهيم","عبود","عزيز","زهير","محمود",);
 $url = "http://www.123.com/login?username=".$arrayName[array_rand($arrayName)];
 $html = file_get_html($url);
@@ -8,9 +8,10 @@ $new = $html->find("div.alert3");
 if ($new) {
     echo "FALSE"."\n";
     echo $url;
-    echo "<br>";
+    echo "\n";
 }else{
-	echo "Done";
+	echo "Done"."\n";
+echo $url;
 }
-}
+}die
 ?>
