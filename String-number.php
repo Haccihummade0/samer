9bedcfbb8928fd2d11516dbc551681275837c9be
Haccihummade0 <77897693+Haccihummade0@users.noreply.github.com>
2022-01-22 17:50:48 +0200

    Create String-number.php

---
+++ String-number.php
@@ -0,0 +1,23 @@
+<?php
+require 'simple_html_dom.php';
+
+if (isset($_GET["url"])) {
+	$a = $_GET["url"];
+}else{
+	$a = 123;
+}
+
+for ($i=0; $i < 300; $i++) { 
+$rand = rand(1,9);
+
+$url = "http://www.".$a.".com/login?username=".$rand;
+$html = file_get_html($url);
+$new = $html->find("div.alert3");
+if ($new) {
+    echo "FALSE"."\n";
+    echo $url;
+    echo "\n";
+}else{
+echo "Done";
+}
+}
